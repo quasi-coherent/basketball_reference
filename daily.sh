f9074f64b928a3d3300ad6970426875deb0f11b7
Daniel Michael Donohue <donohue@earnestresearch.com>
2017-02-25 13:23:06 -0500

    Add timing, new data

--- daily.sh
+++ daily.sh
@@ -2,23 +2,23 @@
 
 set -e -x 
 
-echo "Archiving data directory..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Archiving data directory..."
 for f in `ls data`; do cp data/$f archive/`date +'%Y%m%d' --date='-2 days'`_$f; done
 
-echo "Preparing new data..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Preparing new data..."
 ./prepare_data.py 2>&1
 
-echo "Training models and making predictions..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Training models and making predictions..."
 ./train_and_predict.py 2>&1
 
-echo "Sending emails..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Sending emails..."
 ./send_emails.py 2>&1
 
-echo "Syncing directories with s3..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Syncing directories with s3..."
 aws s3 sync data/ s3://donohue/nba/data/ 2>&1
 aws s3 sync archive/ s3://donohue/nba/archive/snapshot/ 2>&1
 
-echo "Logging..."
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Logging..."
 aws s3 cp /var/log/cloud-init-output.log s3://donohue/nba/log/`date +%Y-%m-%d`.log
 
-echo "Done!"
+echo "$(date +"%Y-%m-%d %H:%M:%S %Z"): Done!"
