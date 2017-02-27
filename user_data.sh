441dbf279e1609449163c1edd276ed22a4778587
Daniel Michael Donohue <donohue@earnestresearch.com>
2017-02-27 11:12:23 -0500

    minor edits

--- user_data.sh
+++ user_data.sh
@@ -4,10 +4,11 @@ apt-get update
 apt-get install -y awscli
 apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 pip install scrapy bs4 pandas numpy scipy scikit-learn
-aws s3 cp --recursive s3://donohue/.aws/ /home/ubuntu/
+mkdir /home/ubuntu/.aws; aws s3 cp --recursive s3://donohue/.aws/ /home/ubuntu/
 aws s3 cp --recursive s3://donohue/nba/project/ /home/ubuntu/
 aws s3 sync s3://donohue/nba/data/ /home/ubuntu/basketball_reference/data/
 chmod 700 /home/ubuntu/basketball_reference/*.py /home/ubuntu/basketball_reference/*.sh
+# chown -R ubuntu /home/ubuntu/basketball_reference/
 echo "export SMTP_ENDPOINT='email-smtp.us-east-1.amazonaws.com'" >> /home/ubuntu/.profile
 echo "export SMTP_PORT=587" >> /home/ubuntu/.profile
 echo "export SMTP_USER='AKIAIHBIB5SQUWF77FUQ'" >> /home/ubuntu/.profile
