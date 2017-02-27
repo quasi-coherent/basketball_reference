0b2b3146a7cedfb603aa0188781a3caba5352d9d
Daniel Michael Donohue <donohue@earnestresearch.com>
2017-02-26 23:36:40 -0500

    add aws configs

--- user_data.sh
+++ user_data.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install -y awscli
 apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
 pip install scrapy bs4 pandas numpy scipy scikit-learn
+aws s3 cp --recursive s3://donohue/.aws/ /home/ubuntu/
 aws s3 cp --recursive s3://donohue/nba/project/ /home/ubuntu/
 aws s3 sync s3://donohue/nba/data/ /home/ubuntu/basketball_reference/data/
 chmod 700 /home/ubuntu/basketball_reference/*.py /home/ubuntu/basketball_reference/*.sh
