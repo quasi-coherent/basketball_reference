352a20be9209f957fcb20d73e2b92ba626d06f1a
Daniel Michael Donohue <donohue@earnestresearch.com>
2017-03-12 04:48:32 -0400

    Edit user_data

--- user_data.sh
+++ user_data.sh
@@ -7,13 +7,10 @@ pip install scrapy bs4 pandas numpy scipy scikit-learn
 mkdir /home/ubuntu/.aws; aws s3 cp --recursive s3://donohue/.aws/ /home/ubuntu/.aws/
 aws s3 cp --recursive s3://donohue/nba/project/ /home/ubuntu/
 aws s3 sync s3://donohue/nba/data/ /home/ubuntu/basketball_reference/data/
-chmod 700 /home/ubuntu/basketball_reference/*.py /home/ubuntu/basketball_reference/*.sh
-# chown -R ubuntu /home/ubuntu/basketball_reference/
-echo "export SMTP_ENDPOINT='email-smtp.us-east-1.amazonaws.com'" >> /home/ubuntu/.profile
-echo "export SMTP_PORT=587" >> /home/ubuntu/.profile
-echo "export SMTP_USER='AKIAIHBIB5SQUWF77FUQ'" >> /home/ubuntu/.profile
-echo "export SMTP_PASS='AmuZFyh/kL5l1lunYlVA3DNX1eWUGj0Eii9UMYFrthP0'" >> /home/ubuntu/.profile
+chmod 700 /home/ubuntu/basketball_reference/*.py
 source /home/ubuntu/.profile
 cd /home/ubuntu/basketball_reference/
-./daily.sh
+./scrape_daily_boxscores.py 
+./train.py
+aws s3 cp --recursive resources/ s3://donohue/nba/models/
 aws ec2 terminate-instances --instance-ids `ec2metadata --instance-id`
