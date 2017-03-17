11c80798580df13c5046eb204f61bcb267d8037d
Daniel Michael Donohue <donohue@earnestresearch.com>
2017-03-17 10:48:57 -0400

    Typo

--- models/preprocess.py
+++ models/preprocess.py
@@ -67,10 +67,10 @@ class PreprocessBoxscore(object):
           away_df[away_df["team"] == away_team]]).drop(["team", "date"], axis=1)
     home_season, home_last_ten, home_last_three, home_last_five, home_last_one, home_at_home = \
       self.get_averages(home_team_df), self.get_averages(home_team_df, 10), self.get_averages(home_team_df, 5), self.get_averages(home_team_df, 3), self.get_averages(home_team_df, 1)\
-      self.get_averages(home_df[home_df["team"] == home_team].sort_values(by="date").drop(["team", "date"], axis=1))
+      self.get_averages(home_df[home_df["team"] == home_team].drop(["team", "date"], axis=1))
     away_season, away_last_ten, away_last_five, away_last_three, away_last_one, away_at_away = \
       self.get_averages(away_team_df), self.get_averages(away_team_df, 10), self.get_averages(away_team_df, 5), self.get_averages(away_team_df, 3), self.get_averages(home_team_df, 1)\
-      self.get_averages(away_df[away_df["team"] == away_team].sort_values(by="date").drop(["team", "date"], axis=1))
+      self.get_averages(away_df[away_df["team"] == away_team].drop(["team", "date"], axis=1))
     home_input = 0.1*home_season + 0.25*home_last_ten + 0.35*home_last_five + 0.15*home_last_three + 0.05*home_last_one + 0.1*home_at_home
     away_input = 0.1*away_season + 0.25*away_last_ten + 0.35*home_last_five + 0.15*away_last_three + 0.05*away_last_one + 0.1*away_at_away
     input_ = away_input.append(home_input)
