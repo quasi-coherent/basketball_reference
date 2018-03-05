cf8418da84d8f4b91480b8df05009e4397bd2a1e
Daniel Donohue <donohue@earnestresearch.com>
2018-03-04 21:54:36 -0500

    Wrong error message

--- scrape_monthly_boxscores.py
+++ scrape_monthly_boxscores.py
@@ -7,7 +7,7 @@ from util.util import launch_spider, prepare_dates
 try:
   _, month, season = prepare_dates(sys.argv[1])
 except IndexError:
-  sys.stdout.write("Example usage: python scrape_monthly_boxscores.py 2017 may")
+  sys.stdout.write("Example usage: python scrape_monthly_boxscores.py 20170501")
   sys.exit(1)
 
 project_dir = os.environ["PROJECT_DIR"]
