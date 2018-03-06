c10b448a0a53f54f33adc9d06c06c57c8aa1afeb
Daniel Donohue <donohue@earnestresearch.com>
2018-03-06 00:38:04 -0500

    Proper logging

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ from models.preprocess import PreprocessBoxscore
 from models.spread_and_total import SpreadAndTotalRegressor
 from models.params import *
 
-now = datetime.datetime.now()
 season = now.year if now.month in range(1, 7) else now.year + 1
 
 # inputs
@@ -28,7 +27,7 @@ X_spread, y_spread = pp.spread()
 GBR_spread = GradientBoostingRegressor(**SPREAD_PARAMS)
 SR = SpreadAndTotalRegressor(features=X_spread, response=y_spread,
   model=GBR_spread)
-sys.stdout.write("[%s]: Training spread regressor...\n" % now.strftime("%x %X"))
+sys.stdout.write("[%s]: Training spread regressor...\n" % datetime.datetime.now().strftime("%x %X"))
 sys.stdout.flush()
 SR.train()
 joblib.dump(SR, "resources/SR.pkl")
@@ -37,7 +36,7 @@ X_total, y_total = pp.total()
 GBR_total = GradientBoostingRegressor(**TOTAL_PARAMS)
 TR = SpreadAndTotalRegressor(features=X_total, response=y_total,
   model=GBR_total)
-sys.stdout.write("[%s]: Training total regressor...\n" % now.strftime("%x %X"))
+sys.stdout.write("[%s]: Training total regressor...\n" % datetime.datetime.now().strftime("%x %X"))
 sys.stdout.flush()
 TR.train()
 joblib.dump(TR, "resources/TR.pkl")
@@ -46,7 +45,7 @@ X_moneyline, y_moneyline = pp.moneyline()
 GBC_moneyline = GradientBoostingClassifier(**MONEYLINE_PARAMS)
 ML = MoneylineClassifier(features=X_moneyline, response=y_moneyline,
   model=GBC_moneyline)
-sys.stdout.write("[%s]: Training moneyline classifier...\n" % now.strftime("%x %X"))
+sys.stdout.write("[%s]: Training moneyline classifier...\n" % datetime.datetime.now().strftime("%x %X"))
 sys.stdout.flush()
 ML.train()
 joblib.dump(ML, "resources/ML.pkl")
